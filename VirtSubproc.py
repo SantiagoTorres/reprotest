b72c78414e5235a76cf4a4025582240366005656
Martin Pitt <martin.pitt@ubuntu.com>
2015-01-05 15:25:57 +0100

    VirtSubproc.cmd_reboot: Avoid multi-line shell command
    So that this also gets along with the ssh runner.

--- VirtSubproc.py
+++ VirtSubproc.py
@@ -311,20 +311,21 @@ def cmd_reboot(c, ce):
     # save current downtmp; try a few locations, as /var/cache might be r/o
     # (argh Ubuntu touch)
     directories = '/var/cache /home'
-    check_exec(['sh', '-ec', '''for d in %s; do if [ -w $d ]; then
-              tar --create --absolute-names -f $d/autopkgtest-tmpdir.tar '%s';
-              exit 0; fi; done; exit 1
-        ''' % (directories, downtmp)], downp=True, timeout=copy_timeout)
+    check_exec(['sh', '-ec', 'for d in %s; do if [ -w $d ]; then '
+                'tar --create --absolute-names -f $d/autopkgtest-tmpdir.tar'
+                ''' '%s'; exit 0; fi; done; exit 1''' %
+                (directories, downtmp)],
+               downp=True, timeout=copy_timeout)
     adtlog.debug('cmd_reboot: saved current downtmp, rebooting')
 
     caller.hook_reboot()
 
     # restore downtmp
-    check_exec(['sh', '-ec', '''for d in %s; do
-        if [ -e $d/autopkgtest-tmpdir.tar ]; then
-           tar --extract --absolute-names -f $d/autopkgtest-tmpdir.tar;
-           rm $d/autopkgtest-tmpdir.tar; exit 0;
-        fi; done; exit 1''' % directories],
+    check_exec(['sh', '-ec', 'for d in %s; do '
+                'if [ -e $d/autopkgtest-tmpdir.tar ]; then '
+                ' tar --extract --absolute-names -f $d/autopkgtest-tmpdir.tar;'
+                ' rm $d/autopkgtest-tmpdir.tar; exit 0; '
+                'fi; done; exit 1' % directories],
                downp=True, timeout=copy_timeout)
     adtlog.debug('cmd_reboot: saved current downtmp, rebooting')
 
