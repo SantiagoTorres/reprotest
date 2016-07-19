f2c309e9409ca699ff358fcf371e1142e322781e
Ceridwen <ceridwenv@gmail.com>
2016-07-19 00:13:18 -0400

    Shorten license boilerplate and add new copyright info

--- reprotest/lib/VirtSubproc.py
+++ reprotest/lib/VirtSubproc.py
@@ -1,24 +1,5 @@
-# VirtSubproc is part of autopkgtest
-# autopkgtest is a tool for testing Debian binary packages
-#
-# autopkgtest is Copyright (C) 2006-2007 Canonical Ltd.
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-#
-# See the file CREDITS for a full list of credits information (often
-# installed as /usr/share/doc/autopkgtest/CREDITS).
+# Licensed under the GPL: https://www.gnu.org/licenses/gpl-3.0.en.html
+# For details: reprotest/debian/copyright
 
 import __main__
 
