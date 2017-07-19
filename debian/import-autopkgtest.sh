68ecf09671e868b4ae320d4c036bd8570467db5e
Ximin Luo <infinity0@debian.org>
2017-07-19 17:07:42 +0200

    Add script for importing autopkgtest code

---
+++ debian/import-autopkgtest.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+set -e
+
+tag=${1:-4.4}
+repo=${2:-../autopkgtest}
+
+( cd "$repo" && git checkout "$tag" )
+
+lib_imports="adt_testbed adtlog VirtSubproc"
+
+ourpatch() {
+	for i in $lib_imports; do
+		sed -i -e 's/^import '"$i"'/from reprotest.lib import '"$i"'/g' "$1"
+	done
+}
+
+last_import=$(git log --pretty="format:%H" --grep='Import autopkgtest')
+
+for i in $lib_imports; do
+	target=reprotest/lib/"$i.py"
+	cp "$repo/lib/$i.py" "$target"
+	ourpatch "$target"
+done
+
+for i in "$repo/virt/"*; do
+	if [ "$i" != "${i%.1}" ]; then continue; fi # man page
+	target=reprotest/virt/$(basename "$i")
+	cp "$i" "$target"
+	ourpatch "$target"
+done
+
+echo "*** Import complete; run the following to commit it:"
+echo "  git commit -m 'Import autopkgtest $tag' reprotest/lib/ reprotest/virt/"
+echo "*** Afterwards, you should re-import the following patches:"
+git log --oneline "${last_import}..HEAD" -- reprotest/lib/ reprotest/virt/
